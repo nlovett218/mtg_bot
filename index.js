638c8261ac0803593b698e958d7ef5bc568a2617
Koty Glover <kotyg1995@gmail.com>
2019-03-05 19:01:32 -0500

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const Client = new Discord.Client();
 const config = require('./config.json');
+const Commando = require('discord.js-commando');
+const fileUrl = require('file-url');
 
 
 Client.on('ready', () => {
@@ -9,6 +11,33 @@ Client.on('ready', () => {
     Client.channels.find(x => x.name === 'ragebot-testing').send('Rage\'s bot is online.');
 });
 
+Client.on('message', function(message){
+    if (message.content === '!draw' ){
+  
+      const embed = new Discord.RichEmbed()
+      fileUrl("C:\Users\Koty Glover\Documents\GitHub\mtg_bot\img1.PNG")
+        }
+ });
+
+      Client.on('message', message => {
+        // Voice only works in guilds, if the message does not come from a guild,
+        // we ignore it
+        if (!message.guild) return;
+      
+        if (message.content === '!join') {
+          // Only try to join the sender's voice channel if they are in one themselves
+          if (message.member.voiceChannel) {
+            message.member.voiceChannel.join()
+              .then(connection => { // Connection is an instance of VoiceConnection
+                message.reply('Trump is in the house!');
+              })
+              .catch(console.log);
+          } else {
+            message.reply('You need to join a voice channel first!');
+          }
+        }
+ });
+    
 
 Client.on('message', function(message){
     if(message.content == 'Hello')
